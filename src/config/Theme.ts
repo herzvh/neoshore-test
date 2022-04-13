a063061254bbab8defddb33f44ce4c3cfce4f0e0
herz <herizovonjinirina@gmail.com>
2022-04-13 19:26:19 +0300

    Initial commit

---
+++ src/config/Theme.ts
@@ -0,0 +1,19 @@
+import { ThemeOptions } from "@mui/system"
+
+const primary = {
+    main: '#d319af',
+    light: '#ffffff'
+}
+
+const secondary = {
+    main: '#24252d',
+    light: '#ffffff'
+}
+
+
+export const Theme: ThemeOptions = {
+    palette: {
+        primary,
+        secondary
+    }
+}
